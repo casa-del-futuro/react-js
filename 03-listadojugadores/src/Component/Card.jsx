d45c9e498033444034a7e6b82dce8bd12d9b8db8
lucaszhh <lucaszarandon24@gmail.com>
2023-11-11 11:59:02 -0300

    add (3-listadoJugadores) add clase 3 player football cards

---
+++ 03-listadojugadores/src/Component/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Card = ({jugador}) => {
+    const {name,position, number} = jugador
+    return (
+        <div class="e-card playing">
+            <div class="image"></div>
+
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+
+            <div class="infotop">
+                <h1>{number}</h1>
+                <h2>{name}</h2>
+                <div class="name">{position}</div>
+            </div>
+        </div>
+    )
+}
+export default Card
---
+++ 03-listadojugadores/src/Component/ListCard.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import Card from './Card'
+
+const ListCard = () => {
+
+    const [list, setList] = useState([
+        {
+          name: "Lionel Messi",
+          position: "Forward",
+          number: "10"
+        },
+        {
+          name: "Cristiano Ronaldo",
+          position: "Forward",
+          number: "7"
+        },
+        {
+          name: "Neymar Jr",
+          position: "Forward",
+          number: "10"
+        },
+        {
+          name: "Kylian Mbappe",
+          position: "Forward",
+          number: "7"
+        },
+        {
+          name: "Robert Lewandowski",
+          position: "Forward",
+          number: "9"
+        },
+        {
+          name: "Kevin De Bruyne",
+          position: "Midfielder",
+          number: "17"
+        },
+        {
+          name: "Sergio Ramos",
+          position: "Defender",
+          number: "4"
+        },
+        {
+          name: "Virgil van Dijk",
+          position: "Defender",
+          number: "4"
+        },
+        {
+          name: "Manuel Neuer",
+          position: "Goalkeeper",
+          number: "1"
+        },
+        {
+          name: "Mohamed Salah",
+          position: "Forward",
+          number: "11"
+        }
+      ])
+
+    return (
+        <>
+            {list.map((jugador, index)=>{
+                return(<Card key={index} jugador={jugador} />)
+            })}
+        </>
+    )
+}
+
+export default ListCard
